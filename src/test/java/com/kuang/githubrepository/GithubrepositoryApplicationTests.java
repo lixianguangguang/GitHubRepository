01e09af4f4c05bbc301dfa1b8dd045f9105392fa
lixianguangguang <664159787@qq.com>
2020-06-18 11:44:33 +0800

    第一次使用idea提交文件

---
+++ src/test/java/com/kuang/githubrepository/GithubrepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuang.githubrepository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubrepositoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
